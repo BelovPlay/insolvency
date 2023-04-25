8b1bc754d582a2642d0151b0e8a03364db1e7e29
Milkov Danil Vladimirovich <valerij.belusin@gmail.com>
2023-04-25 17:37:51 +0300

    new message

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
 let buttonGetConsult=document.getElementById('get-consult');
 let modalwindow=document.getElementById('modal-window-get-consult__content');
+let bgmodalwindow=document.getElementById('modal-window-get-consult');
+let overlay=document.getElementById('overlay');
 buttonGetConsult.addEventListener('click',function func(){
     event.preventDefault();
     modalwindow.style.display='block';
+    modalwindow.classList.add('overlay');
+    overlay.style.display='block';
 })
