294b38636dce57f04917d10d1e392cfb9d1b824f
Milkov Danil Vladimirovich <valerij.belusin@gmail.com>
2023-04-26 17:58:53 +0300

    added code js

--- script/script.js
+++ script/script.js
@@ -1,72 +1,38 @@
-// Открытие закытие модального окна для кнопки заказать звонок
-let getCall=document.getElementById('contacts__link-get-call');
-let ModalWindowGetCall=document.getElementById('modal-window-get-call');
-let closeModalGetCall=document.getElementById('close-modal-window-get-call');
-getCall.addEventListener('click',function func(){ 
+//Модальное окна заказа звонка
+let openModalWindowGetCall=document.getElementById('contacts__link-get-call'); // Ссылка для открытия модального окна заказа звонка
+let modalWindowGetCall=document.getElementById('modal-window-get-call'); // Модальное окно заказа звонка
+let closeModalWindowGetCall=document.getElementById('close-modal-window-get-call'); // Кнопка для закрытия модального окна
+let MobileNumberModalWindowGetCall=document.getElementById('modal-window__input-get-call'); // поле для ввода мобильного телефона
+let BtnGetCall=document.getElementById('modal-window__get-call'); // кнопка для заявки
+let phoneNumbersForCalling=[]; // номера телефонов для обзвона 
+let ModalWindowSuccces=document.getElementById('modal-window-get-receiving-request'); // окно успешного получения телефонного номера
+let BtnSuccess=document.getElementById('modal-window-get-receiving-request__link'); // кнопка подтверждения успешной заявки
+// Функция открытия модального окна
+openModalWindowGetCall.addEventListener('click',function(){
     event.preventDefault();
-    ModalWindowGetCall.style.display='block';
+    modalWindowGetCall.style.display='block';
 })
-closeModalGetCall.addEventListener('click',function(){
-    ModalWindowGetCall.style.display='none';
+// Функция закрытия модального окна
+closeModalWindowGetCall.addEventListener('click',function(){
+    modalWindowGetCall.style.display='none';
 })
-// Валидация формы заказа звонка
-let getCalls=[];
-let phoneNumberGetCall=document.getElementById('modal-window__input-get-call');
-let getNumberGetCall=document.getElementById('modal-window__get-call');
-getNumberGetCall.addEventListener('click',function func(){
+// Функция отправки номера в массив при прохождении валидации
+BtnGetCall.addEventListener('click',function(){
     event.preventDefault();
-    if(((phoneNumberGetCall.value[0]==='7')||(phoneNumberGetCall.value[0]==='8')||(phoneNumberGetCall.value[0]==='+' && phoneNumberGetCall.value[1]==='7'))&&(phoneNumberGetCall.value.length<13)){
-        getCalls.push(phoneNumberGetCall.value);
-        phoneNumberGetCall.value='';
-        console.log(getCalls);
-    }else{
-        alert('Проверьте правильность введеных данных');
-        phoneNumberGetCall.value='';
+    if(MobileNumberModalWindowGetCall.value[0]==8 && MobileNumberModalWindowGetCall.value.length<12){
+        phoneNumbersForCalling.push(MobileNumberModalWindowGetCall.value);
+        console.log(phoneNumbersForCalling);
+        ModalWindowSuccces.style.display='block';
     }
+    // else if(){
+
+    // }else{
+    //     alert('Проверь корректность введеных данных');
+    // }
 })
-// Модальное окно получения консультации в главной секции
-// Открытие закрытие модального окна получения консультации
-let GetConsult=document.getElementById('get-consult'); 
-let ModalWindowGetConsult=document.getElementById('modal-window-get-consult');
-let closeModalGetConsult=document.getElementById('close-modal-window');
-GetConsult.addEventListener('click',function func(){
+BtnSuccess.addEventListener('click',function(){
     event.preventDefault();
-    ModalWindowGetConsult.style.display='block';
-    phoneNumberGetCall.value='';
-})
-closeModalGetConsult.addEventListener('click',function(){
-    ModalWindowGetConsult.style.display='none';
-    phoneNumberGetCall.value='';
-})
-// Валидация формы получения консультации
-let numbers=[];
-let phoneNumber=document.getElementById('modal-window__input');
-let getNumber=document.getElementById('modal-window__get-consult');
-getNumber.addEventListener('click',function func(){
-    event.preventDefault();
-    if(((phoneNumber.value[0]==='7')||(phoneNumber.value[0]==='8')||(phoneNumber.value[0]==='+' && phoneNumber.value[1]==='7'))&&(phoneNumber.value.length<13)){
-        numbers.push(phoneNumber.value);
-        ModalWindowRecievingRequest.style.display='block';
-        bgModalWindowRecieving.style.display='block';
-        ModalWindowGetConsult.style.display='none';
-        console.log(numbers);
-        phoneNumber.value='';
-    }else{
-        alert('Проверьте правильность введеных данных');
-    }
-})
-// Модальное окно получения заявки от клиента
-// Открытие закрытие модального окна получения заявки от клиента
-let BtnCloseModalWindow=document.getElementById('modal-window-get-receiving-request__link');
-let ModalWindowRecievingRequest=document.getElementById('modal-window-get-receiving-request__content');
-let bgModalWindowRecieving=document.getElementById('modal-window-get-receiving-request');
-let closeModalConfirmRecieving=document.getElementById('close-modal-window-recieving-modal');
-BtnCloseModalWindow.addEventListener('click',function func(){
-    event.preventDefault();
-    ModalWindowRecievingRequest.style.display='none';
-    bgModalWindowRecieving.style.display='none';
-})
-closeModalConfirmRecieving.addEventListener('click',function(){
-    ModalWindowRecievingRequest.style.display='none';
-    bgModalWindowRecieving.style.display='none';
+    modalWindowGetCall.style.display='none';
+    ModalWindowSuccces.style.display='none';
+    MobileNumberModalWindowGetCall.value='';
 })
